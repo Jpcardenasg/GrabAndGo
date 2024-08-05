0ce55963bb983048491678fc1e020572d75befe7
JpCardenasG <jpcardenasgonzalez.com>
2024-08-05 18:22:03 -0500

    style: :lipstick: Toggle SideBar

--- src/app/admin-page/admin-page.component.ts
+++ src/app/admin-page/admin-page.component.ts
@@ -1,18 +1,27 @@
-import { Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { ProductManagementComponent } from './product-management/product-management.component';
 
 @Component({
-  selector: 'app-admin-page',
-  standalone: true,
-  imports: [SidebarComponent, ProductManagementComponent],
-  templateUrl: './admin-page.component.html',
-  styleUrl: './admin-page.component.css'
+	selector: 'app-admin-page',
+	standalone: true,
+	imports: [SidebarComponent, ProductManagementComponent],
+	templateUrl: './admin-page.component.html',
+	styleUrl: './admin-page.component.css'
 })
-export class AdminPageComponent {
-  @ViewChild(SidebarComponent) sidebarComponent!: SidebarComponent;
 
-  toggleSidebar() {
-    this.sidebarComponent.toggleSidebar();
-  }
+export class AdminPageComponent implements AfterViewInit {
+	@ViewChild('sidebarContainer', { static: false }) sidebarContainer!: ElementRef;
+	isCollapsed = false;
+	constructor(private renderer: Renderer2) { }
+
+	ngAfterViewInit() {
+		if (!this.sidebarContainer) {
+			console.error('sidebarContainer no está inicializado');
+		}
+	}
+
+	toggleSidebar() {
+		this.isCollapsed = !this.isCollapsed;
+	}
 }
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -8,19 +8,5 @@ import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
-  @ViewChild('sidebar', { static: false }) sidebar!: ElementRef;
-
-  constructor(private renderer: Renderer2) { }
-
-  toggleSidebar() {
-    console.log('entra');
-
-    const sidebar = this.sidebar.nativeElement;
-    if (sidebar.classList.contains('-translate-x-full')) {
-      this.renderer.removeClass(sidebar, '-translate-x-full');
-    } else {
-      this.renderer.addClass(sidebar, '-translate-x-full');
-    }
-  }
 
 }
